950401cb401597a646e16c41cf51262b5aa6a50d
Trodrige <tigrodrige@gmail.com>
2016-04-26 12:46:50 +0100

    few changes

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -9,12 +9,15 @@
 
                 <div class="panel-body">
                     <div class="row" id="row1">
-                        <div class="col-sm-4">
-                            <img src="../../../uploads/images/{{$user->picture}}" alt="Profile picture"
+                        <div class="col-sm-2">
+                            <div id="img"><img src="../../../uploads/images/{{$user->picture}}" alt="Profile picture"
                                  width="100px" height="20px" class="img-responsive">
-                            <a href="{{url('/user/edit')}}/{{$user->firstname}}">Change profile picture</a>
+                            </div>
+                            <a href="{{url('/user/edit')}}/{{$user->firstname}}">
+                                <h5 style="text-align:center">Change profile picture</h5>
+                            </a>
                         </div>
-                        <div class="col-sm-8" id="credentials">
+                        <div class="col-sm-10" id="credentials">
                             <h5><b>Name</b>: {{$user->firstname}} {{$user->lastname}}</h5>
                             <h5><b>email</b>: {{$user->email }}</h5>
                             <h5><b>Location</b>: Buea, Cameroon</h5>
@@ -22,11 +25,21 @@
                         </div>
                     </div><br>
 
-                    <div class="row2">
-
-                        <h5 class="lead" id="personal-info">Posts</h5>
-                        <h4>Display posts here</h4>
-
+                    <div class="row" >
+                        <div class="panel-section panel-default">
+                            <div class="panel-heading" id="profile-header">My Posts</div>
+                            <div class="panel panel-body" >
+                                <article>
+                                    <p class="lead">OOP and design pattern in PHP 5</p>
+                                        This can be solved by defining, withing your
+                                    class, a static function
+                                    getInstance() which
+                                    creates and stores an object instance in a private
+                                    member of the class on it's first call while
+                                    subsequent call returns this unique instance
+                                </article>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
